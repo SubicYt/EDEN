ba65b4ab895bf12831849dae4c6701f5f434df08
Ethan Denning <96307838+SubicYt@users.noreply.github.com>
2025-08-31 23:39:29 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include "lexer.h"
 
-//vscode is boy soyboys.
+//vscode is for soyboys.
 
 
 //supporting commandline agruments idk how ts works bro. 
@@ -16,24 +16,29 @@ int main(int argc, char* argv[]) {
 		return EXIT_FAILURE; 
 	}
 	
-	std::fstream inputCode(argv[1], std::ios::in);//specified usage. just read in;
+	std::fstream inputCode(argv[1], std::ios::in); //specified usage. just read in;
+    if(!inputCode.is_open()){
+        std::cerr<<"Could not open source code file";
+        return EXIT_FAILURE;
+    }
+    
 	std::stringstream contentsStream;
 	contentsStream << inputCode.rdbuf();
 	inputCode.close();
 	
 	std::string sourceCode = contentsStream.str(); // Convert stringstream to a string
-	std::ofstream output("./out.txt");
+	std::ofstream output("TokensOut.txt");
 
 	lexer Lexer(sourceCode);
 	std::vector<Token> sourceTokens;
 	sourceTokens = Lexer.tokenize();
 
-	for (int i = 0; i < sourceTokens.size(); i++) {
+	for (size_t i = 0; i < sourceTokens.size(); i++) {
 		output << "{" << tokenTypeToString(sourceTokens[i].type)
 			<< ", " << sourceTokens[i].tokenValue << "}" << ", ";
 	}
 
 	output.close();
-		
+
 	return EXIT_SUCCESS;
 } 
